20022c603b8c2ecac20e1935d99e3ba4dd0620bd
DJDrama <putzzang@naver.com>
2025-01-10 18:32:21 +0900

    Firebase Dependency

---
+++ WhatsUp/WhatsUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsUpApp.swift
+//  WhatsUp
+//
+//  Created by Dongjun Lee on 1/10/25.
+//
+
+import SwiftUI
+
+@main
+struct WhatsUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
