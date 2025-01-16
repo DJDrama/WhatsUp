bdb3d89be22b202a26bb4ff63f7e1b8efb0b42d6
DJDrama <putzzang@naver.com>
2025-01-16 14:57:12 +0900

    Chat Bubble Implementation Part 2

--- WhatsUp/Views/ChatMessageListView.swift
+++ WhatsUp/Views/ChatMessageListView.swift
@@ -6,13 +6,36 @@
 //
 
 import SwiftUI
+import FirebaseAuth
 
 struct ChatMessageListView: View {
     let chatMessages: [ChatMessage]
+    
+    private func isChatMessageFromCurrentUser(_ chatMessage: ChatMessage) -> Bool {
+        guard let currentUser = Auth.auth().currentUser else {
+            return false
+        }
+        return currentUser.uid == chatMessage.uid
+    }
+    
     var body: some View {
         List(chatMessages) { chatMessage in
-            ChatMessageView(chatMessage: chatMessage, direction: .right, color: .blue)
+            VStack{
+                if isChatMessageFromCurrentUser(chatMessage) {
+                    HStack{
+                        Spacer()
+                        ChatMessageView(chatMessage: chatMessage, direction: .right, color: .blue)
+                    }
+                }else {
+                    HStack{
+                        ChatMessageView(chatMessage: chatMessage, direction: .left, color: .gray)
+                        Spacer()
+                    }
+                }
+                Spacer().frame(height: 20)
+            }.listRowSeparator(.hidden)
         }.listStyle(.plain)
+            
     }
 }
 
